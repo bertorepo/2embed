cecbded077c3808abbdfd862778c74befb2f55d2
Hubert Berja <bert.stark3231@gmail.com>
2023-03-26 12:51:41 +0800

    added new list of movies/series

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,5 +1,5 @@
-// const MAIN_URL = "https://2embed-server.vercel.app/api";
-const MAIN_URL = "http://localhost:3001";
+const MAIN_URL = "https://2embed-server.vercel.app/api";
+// const MAIN_URL = "http://localhost:3001";
 const OMDB_URL = "http://www.omdbapi.com";
 const API_KEY = "9931b7c";
 
